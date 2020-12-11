17abcb525340d546c005e5fc30750123725e92b4
WillV1 <wmvanhook@gmail.com>
2020-12-11 05:02:44 -0500

    Update blog model

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -9,6 +9,7 @@ class Blog(models.Model):
     null=True)
     title = models.CharField(max_length=100)
     text = models.TextField()
+    category = models.CharField(max_length=50)
     date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='images/', blank=True)
 
