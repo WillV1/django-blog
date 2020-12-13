070a545de694c1860537086565fe841944b68622
WillV1 <wmvanhook@gmail.com>
2020-12-13 16:31:40 -0500

    Update dashboard

--- frontend/src/pages/DashBoard.js
+++ frontend/src/pages/DashBoard.js
@@ -1,11 +1,42 @@
-import React, { Fragment } from 'react';
+import React, { useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layouts/Spinner';
+import { getProfile } from '../actions/profiles';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
-const DashBoard = () => {
-  return (
+const DashBoard = ({getProfile, profile: {profile, loading}, auth: {user}}) => {
+
+  useEffect(() => {
+    getProfile()
+  }, [getProfile]);
+
+  return ( 
+    <Container>
+  {!loading && profile !== null ? 
     <Fragment>
-      DashBoard
-    </Fragment>
+      <h2>Welcome {user && user.username}</h2>
+      </Fragment> : 
+    <Fragment>
+      <h2>Welcome {user && user.username}</h2>
+      <p>Create a profile here to start blogging!</p>
+      <Link to="/addprofile">
+        <Button variant="primary">Create Profile</Button>
+      </Link>
+    </Fragment>}
+  </Container>
   )
 }
 
-export default DashBoard;
+DashBoard.propTypes = {
+  getProfile: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profiles
+})
+
+export default connect(mapStateToProps, { getProfile })(DashBoard);
